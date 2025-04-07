cedfa32166f9197f129c38a8713f0eb0ab563825
Alessandra Rebecchi <alessandrarebecchi1@gmail.com>
2025-04-07 10:22:22 -0300

    Novo jogo

---
+++ main.js
@@ -0,0 +1,105 @@
+let personagensPossiveis = {
+    "Tókio": "images/tokio.jpg",
+    "Rio": "images/rio.jpg",
+    "Nairóbi": "images/nairobi.jpg",
+    "Berlim": "images/berlim.jpg",
+    "Professor": "images/professor.jpg",
+    "Lisboa": "images/lisboa.jpg"
+};
+
+let viloesPossiveis = {
+    "Damon": "images/damon.jpg",
+    "Stefan": "images/stefan.jpg",
+    "Elena": "images/elena.jpg",
+    "Caroline": "images/caroline.jpg",
+    "Bonnie": "images/bonnie.jpg",
+    "Katerina": "images/katerina.jpg",
+    "Enzo": "images/enzo.jpg",
+    "Tyler": "images/tyler.jpg",
+    "Klaus": "images/klaus.jpg",
+    "Rebeka": "images/rebeka.jpg"
+};
+
+let personagem = [];
+let viloes = [];
+let forcaPersonagem = 0;
+let forcaViloes = 0;
+
+function iniciarJogo() {
+    personagem = [];
+    viloes = [];
+    forcaPersonagem = 0;
+    forcaViloes = 0;
+
+    document.getElementById("seuTime").innerHTML = "";
+    document.getElementById("viloesTime").innerHTML = "";
+    document.getElementById("resultado").innerText = "";
+
+    // Escolha dos personagens pelo usuário
+    for (let i = 0; i < 3; i++) {
+        let escolhaPersonagem;
+        do {
+            escolhaPersonagem = prompt("Escolha seu personagem " + (i + 1) +
+                " entre: " + Object.keys(personagensPossiveis).join(", "));
+
+            if (!personagensPossiveis[escolhaPersonagem]) {
+                alert("Escolha inválida! Tente novamente.");
+            } else if (personagem.includes(escolhaPersonagem)) {
+                alert("Este personagem já foi escolhido! Escolha outro.");
+            }
+        } while (!personagensPossiveis[escolhaPersonagem] || personagem.includes(escolhaPersonagem));
+
+        personagem.push(escolhaPersonagem);
+        forcaPersonagem += Math.floor(Math.random() * 10) + 1;
+        adicionarImagem("seuTime", escolhaPersonagem, personagensPossiveis[escolhaPersonagem]);
+    }
+
+    alert("Você escolheu para o seu time: " + personagem.join(", "));
+
+    // Sorteio dos vilões
+    for (let i = 0; i < 3; i++) {
+        let indiceAleatorio;
+        let vilaoEscolhido;
+
+        do {
+            let chavesViloes = Object.keys(viloesPossiveis);
+            indiceAleatorio = Math.floor(Math.random() * chavesViloes.length);
+            vilaoEscolhido = chavesViloes[indiceAleatorio];
+        } while (viloes.includes(vilaoEscolhido));
+
+        viloes.push(vilaoEscolhido);
+        forcaViloes += Math.floor(Math.random() * 10) + 1;
+        adicionarImagem("viloesTime", vilaoEscolhido, viloesPossiveis[vilaoEscolhido]);
+    }
+
+    alert("Esses são os vampiros que o seu time vai encarar: " + viloes.join(", "));
+
+    // Determinar o vencedor
+    let resultadoTexto = "";
+    if (forcaPersonagem > forcaViloes) {
+        resultadoTexto = "Parabéns! Seu time é mais forte e ganhou a disputa. Sua força foi " + forcaPersonagem;
+    } else if (forcaPersonagem < forcaViloes) {
+        resultadoTexto = "Você perdeu. O outro time venceu com força " + forcaViloes;
+    } else {
+        resultadoTexto = "Empate! Os dois times têm a mesma força.";
+    }
+
+    document.getElementById("resultado").innerText = resultadoTexto;
+}
+
+function adicionarImagem(containerId, nome, caminhoImagem) {
+    let container = document.getElementById(containerId);
+    let div = document.createElement("div");
+    div.classList.add("character");
+
+    let img = document.createElement("img");
+    img.src = caminhoImagem;
+    img.alt = nome;
+
+    let nomePersonagem = document.createElement("p");
+    nomePersonagem.innerText = nome;
+
+    div.appendChild(img);
+    div.appendChild(nomePersonagem);
+    container.appendChild(div);
+}
